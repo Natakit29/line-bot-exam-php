58aded900e574f4da0e1a727c5629fedb36dfa0a
Natakit29 <46317822+Natakit29@users.noreply.github.com>
2019-01-02 18:54:24 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,9 +4,9 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'JizWwVLk+BQCEufLy0LE/OZZL/EZdWw1wzRzMASzu5NwnCAm7huqkkX6ib5slBW+7aap+wokEHKS+P0EBgtW4ptrNmj/uhKw22v5w2aCYDKO1HX84po49aW+SlN5/8oBwYraNOBo1gJ+JEG2m4gVXwdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '18f7d7e38bbde5860af6006223e7273a';
 
 $pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
 
